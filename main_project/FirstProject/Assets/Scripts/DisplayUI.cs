280d0fa442419d5bd3a7793b3592f90c546c4c57
RichardKamau <richard.kamau@hgs.hiddengeniusproject.org>
2021-07-26 17:38:39 -0700

    FifteenthCommit
    A lot.

--- main_project/FirstProject/Assets/Scripts/DisplayUI.cs
+++ main_project/FirstProject/Assets/Scripts/DisplayUI.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class DisplayUI : MonoBehaviour
 {
+	public int value;
 	public GameObject displayObject;
+	CoinManager coinManager;
+	GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {	
+    	gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+        coinManager = gameManager.coinManager;
     }
 
     // Update is called once per frame
@@ -23,7 +27,14 @@ public class DisplayUI : MonoBehaviour
 
     	if(other.tag == "Player")
     	{
+    		GainShoe();
     		displayObject.SetActive(true);
     	}
+    }
+
+    void GainShoe()
+    {
+    	coinManager.coinText.text = "$0";
+    	Destroy(gameObject);  
     }
 }
